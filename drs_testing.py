a0913359639bd160a41a5801fb69a93950e486b3
vflyer009 <mac.joshua@protonmail.com>
2024-07-04 15:02:46 -0400

    Update drs_testing.py

--- drs_testing.py
+++ drs_testing.py
@@ -3,26 +3,33 @@ import pdf_utils
 import selenium_utils as sutils
 
 
-DOWNLOAD_PATH = "/Users/josh.mac/Desktop/drs_test_data"
+DOWNLOAD_PATH = "/Users/$HOME/Desktop/drs_test_data"
 AD_PREFIX_STRING = "<p>"
-PDF_OUTPUT_PATH = "/Users/josh.mac/Desktop/drs_test_data"
+PDF_OUTPUT_PATH = "/Users/$HOME/Desktop/drs_test_data"
 
 
 def main():
     make = "Beechcraft"
     model = "J35"
+    set_limit = "5"
 
-    db_results = db_query.lookup_ad(make, model) 
+    db_results = db_query.lookup_ad(make, model, set_limit) 
+    print(db_results)
 
     if not db_results:
         print("No ADs found in Database")
 
-    drs_request_url = sutils.build_drs_request_url(db_results[0][1])
-    ad_content = sutils.download_ad_content(drs_request_url)
-    ad_html_file = sutils.save_ad_content(ad_content, db_results[0][0], DOWNLOAD_PATH)
+    for ad in db_results:
+        ad_number, unid = ad
+        drs_request_url = sutils.build_drs_request_url(unid)
+        ad_content = sutils.download_ad_content(drs_request_url)
+        ad_html_file = sutils.save_ad_content(ad_content, ad_number, DOWNLOAD_PATH)
 
-    pdf_utils.convert_html_to_pdf(ad_html_file, AD_PREFIX_STRING, DOWNLOAD_PATH)
-    pdf_utils.cleanup_html_file(ad_html_file)
+        if ad_html_file:
+            pdf_utils.convert_html_to_pdf(ad_html_file, AD_PREFIX_STRING, DOWNLOAD_PATH)
+            pdf_utils.cleanup_html_file(ad_html_file)
+        else:
+            print(f"Skipping PDF conversion for AD Number {ad_number} due to missing content.")
 
 
 if __name__ == "__main__":
